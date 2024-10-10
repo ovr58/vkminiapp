aa6f9525c74bdd2cd5602168b55505fdda856188
EVGENII <ovr58armee@yandex.ru>
2024-10-10 15:04:09 +0400

    drizzle out

--- drizzle.config.js
+++
@@ -1,9 +0,0 @@
-const postgresUri = import.meta.env.VITE_APP_DATABASE_URL
-console.log('POSTGRESURI - ', postgresUri)
-export default {
-    schema: "./config/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: postgresUri,
-    }
-  };
