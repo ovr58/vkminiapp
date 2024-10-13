db18465adb15cf019d23c4d5fe80b9b1c8a3b842
EVGENII <ovr58armee@yandex.ru>
2024-10-13 18:00:39 +0400

    srever to replicate

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,9 @@
 import {db} from './db'
 import {chatSession} from './gemeniai'
 import {StoryData} from './schema'
-import { generateImg } from './generateimg'
 
 export {
     db,
     chatSession,
     StoryData,
-    generateImg
 }
